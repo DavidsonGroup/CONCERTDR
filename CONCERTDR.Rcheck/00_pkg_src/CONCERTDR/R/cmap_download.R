0e3b1c60c4441fb7ecd8ed7ccb3a053ecabf9301
zeratulhx <1814737900@qq.com>
2026-03-10 20:43:54 +1100

    remove unnecessary files

--- CONCERTDR.Rcheck/00_pkg_src/CONCERTDR/R/cmap_download.R
+++
@@ -1,2 +0,0 @@
-# Download helpers intentionally live in inst/scripts/download_databases.R
-# so they can be sourced or executed standalone without attaching the package.
--- CONCERTDR.Rcheck/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(CONCERTDR)
-
-test_check("CONCERTDR")
